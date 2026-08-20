1ef9696cfee4b838142dc2ad72f9af8aebb94ede
Pavi93445 <pavithrank607@gmail.com>
2026-08-20 16:30:58 +0530

    connected f and B

--- src/main/java/com/TaskManager/demo/Controller/TaskController.java
+++ src/main/java/com/TaskManager/demo/Controller/TaskController.java
@@ -1,7 +1,6 @@
 package com.TaskManager.demo.Controller;
 
 import com.TaskManager.demo.Entity.TaskEntity;
-import com.TaskManager.demo.Repository.TaskRepository;
 import com.TaskManager.demo.Service.TaskService;
 import org.springframework.web.bind.annotation.*;
 
@@ -9,34 +8,41 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/tasks")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = {
+        "http://localhost:4200",
+        "https://task-manager-frontend-three-snowy.vercel.app"
+})
 public class TaskController {
-   private final TaskService taskService;
 
-   public TaskController(TaskService taskService){
-       this.taskService = taskService;
-   }
-   @PostMapping
-   public TaskEntity createTask(@RequestBody TaskEntity task){
-   return taskService.createTask(task);
-   }
-   @GetMapping
-   public List<TaskEntity> getTasks( ){
-       return taskService.getAllTasks();
-   }
+    private final TaskService taskService;
 
-   @GetMapping("/{id}")
-   public TaskEntity getTaskById(@PathVariable Long id){
-       return taskService.getTaskById(id);
-   }
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
 
-   @PutMapping("/{id}")
-   public TaskEntity updateTask(@PathVariable Long id,@RequestBody TaskEntity task){
-      return taskService.updateTask(id,task);
-   }
+    @PostMapping
+    public TaskEntity createTask(@RequestBody TaskEntity task) {
+        return taskService.createTask(task);
+    }
+
+    @GetMapping
+    public List<TaskEntity> getTasks() {
+        return taskService.getAllTasks();
+    }
+
+    @GetMapping("/{id}")
+    public TaskEntity getTaskById(@PathVariable Long id) {
+        return taskService.getTaskById(id);
+    }
+
+    @PutMapping("/{id}")
+    public TaskEntity updateTask(@PathVariable Long id,
+                                 @RequestBody TaskEntity task) {
+        return taskService.updateTask(id, task);
+    }
 
     @DeleteMapping("/{id}")
-    public void deleteTask(@PathVariable Long id){
-       taskService.deleteTaskById(id);
-   }
+    public void deleteTask(@PathVariable Long id) {
+        taskService.deleteTaskById(id);
+    }
 }
